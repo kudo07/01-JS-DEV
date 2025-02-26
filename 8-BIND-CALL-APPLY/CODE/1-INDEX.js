16a01d6a0ff8c7e36a93a4ff814631bf43e062da
kudo07 <vknwl077@gmail.com>
2025-02-27 01:55:52 +0530

    edit

--- 8-BIND-CALL-APPLY/CODE/1-INDEX.js
+++ 8-BIND-CALL-APPLY/CODE/1-INDEX.js
@@ -1,78 +1,77 @@
 // CALL, APPLY & BIND Interview Question
 
 // Question 1 : What is Call?
-
 function sayHello() {
-  return 'Hello ' + this.name;
+  return 'Hello' + this.name;
 }
 
 var obj = { name: 'Piyush' };
 sayHello.call(obj);
+// obj is the context we put in the function here the function is sayHello()
+// hello piyush
 
 // Question 2 : What is Apply?
-
 function sayHello(day) {
   return 'Hello ' + this.name + ' today is ' + day;
 }
 
-var obj = { name: 'Piyush' };
-sayHello.apply(obj, day);
+var obj = { name: 'Pi' };
+console.log(sayHello.apply(obj, ['Mondayday']));
+// we have to pass the arguments in the form of array
+// otherwise it give createListFromArray called on non-object
 
 // Question 3 : What is Bind?
-
 function sayHello() {
   return 'Hello ' + this.name;
 }
 
 var obj = { name: 'Piyush' };
+console.log(sayHello.bind(obj));
+// bind return a function we have to call this function
+
 const helloFn = sayHello.bind(obj);
 console.log(helloFn());
+// hello Piyush
 
 // Question 4 : Output
-
-const person = { name: 'Piyush' };
-
+const Person = { name: 'iop' };
 function sayHi(age) {
   return `${this.name} is ${age}`;
 }
 
-console.log(sayHi.call(person, 24));
-console.log(sayHi.bind(person, 24));
+console.log(sayHi.call(Person, 24)); //iop is 24
+console.log(sayHi.bind(Person, 24)); //return a function
 
 // Question 5 : Call with function inside object
-
-const age = 10;
+const age = 123;
+// this is distraction
 var person = {
-  name: 'Piyush',
+  name: 'Piyu',
   age: 20,
   getAge: function () {
     return this.age;
   },
 };
-
 var person2 = { age: 24 };
-person.getAge.call(person2);
+console.log(person.getAge.call(person2));
+// 24
 
 // Question 6 : Output
-
 var status = '😎';
-
 setTimeout(() => {
   const status = '😍';
-
   const data = {
     status: '🥑',
     getStatus() {
       return this.status;
     },
   };
-
   console.log(data.getStatus());
-  console.log(data.getStatus.call(this));
+  console.log(data.getStatus.call(this)); //😎
 }, 0);
+// 'this' refers here as the global
 
 // Question 7 : Call printAnimals such that it prints all animals in object
-
 const animals = [
   { species: 'Lion', name: 'King' },
   { species: 'Whale', name: 'Queen' },
@@ -84,29 +83,26 @@ function printAnimals(i) {
   };
   this.print();
 }
-
 for (let i = 0; i < animals.length; i++) {
   printAnimals.call(animals[i], i);
 }
 
 // Question 8 : apply to append an array to another
-
 const array = ['a', 'b'];
 const elements = [0, 1, 2];
 array.push.apply(array, elements);
-console.info(array);
+
+//
 
 // Question 9 - Using apply to enhane built-in functions
 
-// Find min/max number in an array
-const numbers = [5, 6, 2, 3, 7];
-
+const numbers = [12, 32, 1223, 432];
 console.log(Math.max.apply(null, numbers));
 
 // Question 10 : How will you Create a bound function
-
 function f() {
-  alert(this); // ?
+  console.log(this);
+  // ?
 }
 
 let user = {
@@ -114,26 +110,27 @@ let user = {
 };
 
 user.g();
+// this function will pointing to the global
 
 // Question 11 : Bind Chaining?
 
 function f() {
-  alert(this.name);
+  console.log(this.name);
 }
 
 f = f.bind({ name: 'John' }).bind({ name: 'Ann' });
-
+// bind chainig is not happen it only returns 'John'
 f();
 
 // Question 12 : Fix the code
 
 function checkPassword(success, failed) {
-  let password = prompt('Password?', '');
-  if (password == 'Roadside Coder') success();
+  // let password = prompt('Password?', '');
+  let password = 'hello';
+  if (password === 'hello') success();
   else failed();
 }
-
-let user = {
+let userr = {
   name: 'Piyush Agarwal',
 
   loginSuccessful() {
@@ -144,45 +141,60 @@ let user = {
     console.log(`${this.name} failed to log in`);
   },
 };
-
-checkPassword(user.loginSuccessful.bind(user), user.loginFailed.bind(user));
+checkPassword(userr.loginSuccessful.bind(userr), userr.loginFailed.bind(userr));
 
 // Question 13 : Partial application for login
 
 function checkPassword(ok, fail) {
-  let password = prompt('Password?', '');
+  // let password = prompt('Password?', '');
+  let password = 'hello';
   if (password == 'Roadside Coder') ok();
   else fail();
 }
 
-let user = {
+let userOne = {
   name: 'Piyush Agarwal',
 
   login(result) {
     console.log(this.name + (result ? ' login successful' : ' login failed'));
   },
 };
-
-askPassword(?, ?);
+// checkPassword(userOne.login(true), userOne.login(failed));
+checkPassword(
+  userOne.login.bind(userOne, true),
+  userOne.login.bind(userOne, false)
+);
+// userOne.login act as a function and we bind it with the object as the current context
+// thats why this in the userOne had piyush agrawal
 
 // Question 14 :  Explicit Binding with Arrow Function
 
-const age = 10;
+const agee = 10;
 
 var person = {
   name: 'Piyush',
   age: 20,
   getAgeArrow: () => console.log(this.age),
+  // window object again nothing different in arrow function either we use call apply and bind
   getAge: function () {
     console.log(this.age);
+    // 24
   },
 };
-
 var person2 = { age: 24 };
-person.getAge.call(person2);
+console.log(person.getAgeArrow.call(person2));
 person.getAge.call(person2);
 
 // Question 15 : Call Method Polyfill
+
+// dummy
+function displayName() {
+  console.log(this.name);
+  // here we are called
+}
+objectDisplayed = { name: 'here we are called' };
+displayName.call(objectDisplayed);
+// function.call(currentContext,arguments1,arg2,..)
 
 let car1 = {
   color: 'Red',
@@ -204,41 +216,15 @@ function purchaseCar(currency, price) {
     `I have purchased ${this.color} - ${this.company} car for ${currency}${price} `
   );
 }
+// this is the function we are give the context means object
 
-Function.prototype.myCall = function (currentContext = {}, ...arg) {
+Function.prototype.myCall = function (curretContext = {}, ...arg) {
   if (typeof this !== 'function') {
-    throw new Error(this + "it's not callable");
+    throw new Error(this + 'it not callable');
   }
-  currentContext.fn = this;
-  currentContext.fn(...arg);
+  curretContext.fn = this;
+  curretContext.fn(...arg);
 };
-purchaseCar.myCall(car3, '₹', '60,00,000');
+purchaseCar.myCall(car3, '$', '3232');
 
 // Question 16 : Apply Method Polyfill
-
-Function.prototype.myApply = function (currentContext = {}, arg = []) {
-  if (typeof this !== 'function') {
-    throw new Error(this + "it's not callable");
-  }
-  if (!Array.isArray(arg)) {
-    throw new TypeError('CreateListFromArrayLike called on non-object');
-  }
-  currentContext.fn = this;
-  currentContext.fn(...arg);
-};
-purchaseCar.myApply(car2, ['₹', '50,00,000']);
-
-// Question 17 : Bind Method Polyfill
-
-Function.prototype.myBind = function (currentContext = {}, ...arg) {
-  if (typeof this !== 'function') {
-    throw new Error(this + "cannot be bound as it's not callable");
-  }
-  currentContext.fn = this;
-  return function () {
-    return currentContext.fn(...arg);
-  };
-};
-
-const initPurchaseBmw = purchaseCar.myBind(car1, '₹', '1,00,00,000');
-initPurchaseBmw();

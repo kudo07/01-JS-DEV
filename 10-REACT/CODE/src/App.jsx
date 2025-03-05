9e303f777c7e6f765451f05a3577135075c0aa76
kudo07 <vknwl077@gmail.com>
2025-03-05 21:13:04 +0530

    INTERVIEW

--- 10-REACT/CODE/src/App.jsx
+++ 10-REACT/CODE/src/App.jsx
@@ -1,9 +1,11 @@
 import AppReal from './1-REAL-REACT/AppReal';
 import AppRendering from './2-RENDERING-PROCESS/AppRendering';
+import AppInterview from './3-INTERVIEW/AppInterview';
 
 function App() {
   // 1-REAL-REACT return <AppReal />;
-  // 2-RENDERING-PROCESS
-  return <AppRendering />;
+  // 2-RENDERING-PROCESS return <AppRendering />;
+  // 3- INTERVIEW
+  return <AppInterview />;
 }
 export default App;

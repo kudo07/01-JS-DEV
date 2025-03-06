88abfc7aa0440a5d90b209f1526ffb557153b0f2
kudo07 <vknwl077@gmail.com>
2025-03-07 00:11:26 +0530

    5-done

--- 10-REACT/CODE/src/App.jsx
+++ 10-REACT/CODE/src/App.jsx
@@ -2,12 +2,14 @@ import AppReal from './1-REAL-REACT/AppReal';
 import AppRendering from './2-RENDERING-PROCESS/AppRendering';
 import AppInterview from './3-INTERVIEW/AppInterview';
 import AppMapFilter from './4-MAP-FILTER-REDUCE/AppMapFilter';
+import AppClass from './5-CLASS-FUNCITONAL/AppClass';
 
 function App() {
   // 1-REAL-REACT return <AppReal />;
   // 2-RENDERING-PROCESS return <AppRendering />;
   // 3- INTERVIEW return <AppInterview />;
-  // 4-map-filter-REDUCE
-  return <AppMapFilter />;
+  // 4-map-filter-REDUCE return <AppMapFilter />;
+  // 5-CLASS-FUNCTIONAL
+  return <AppClass />;
 }
 export default App;

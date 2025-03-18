ccbecf2a05cf482f097e2ad4894cf16d91db4fa5
kudo07 <vknwl077@gmail.com>
2025-03-18 15:44:18 +0530

    complete

--- 10-REACT/CODE/src/8-HOOKS/2-USE-EFFECT/UseEffectHook.jsx
+++ 10-REACT/CODE/src/8-HOOKS/2-USE-EFFECT/UseEffectHook.jsx
@@ -24,6 +24,26 @@ const UseEffectHook = () => {
   useEffect(() => {
     fetchUser();
   }, []);
+
+  // 5
+  const [seconds, setSeconds] = useState(0);
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setSeconds((prevSec) => prevSec + 1);
+    }, 1000);
+    return () => {
+      clearInterval(intervalId);
+      console.log('intervalcleared');
+    };
+  });
+  // 6
+  useEffect(() => {
+    console.log('useEffect here');
+  }, []);
+
+  useLayoutEffect(() => {
+    console.log('useLayoutEffect here');
+  }, []);
   return (
     <div>
       <h3>
@@ -51,15 +71,33 @@ const UseEffectHook = () => {
       <h5>
         Question 4: Convert major lifecycle methods to useEffect and Explain.
       </h5>
-      {/* Explained in previous lessons */}{' '}
+      {/* Explained in previous lessons If dependencies change, useEffect runs again.*/}{' '}
       <h5>
         Question 5: How to perform cleanup in useEffect? Explain with example.
       </h5>
+      {seconds}
       {/* 
       - You can return a cleanup function inside useEffect, which runs before the effect 
       re-runs or when the component unmounts. 
-      - This is useful for cleaning up subscriptions or event listeners.
+      - This is useful for cleaning up subscriptions or event listeners.f dependencies change, useEffect runs again.
+      Before applying the new effect, React first runs the cleanup function to remove the previous effect (like clearing an event listener or timer).
+      Then, the new effect is executed.
     */}
+      <h5>
+        Question 6: Explain useLayoutEffect and how it is different from
+        useEffect?
+      </h5>
+      {/* 
+      useEffect:
+        - Asynchronous: Runs after the render cycle is committed to the screen.
+        - Good for Performance: Does not block the browser from painting changes on the screen.
+
+      useLayoutEffect:
+        - Synchronous: Runs synchronously immediately after the DOM is updated but before the 
+        browser paints anything on the screen.
+        - Potentially Blocking: Can potentially cause delays in the rendering process if the 
+        operations are heavy.
+      */}
     </div>
   );
 };

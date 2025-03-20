2672bf394ff9311af0fe84d62d7f1500ad1e3175
kudo07 <vknwl077@gmail.com>
2025-03-20 21:55:30 +0530

    2

---
+++ 11-NAMASTEY-JS/1-6-UNDEFINED-VS-DEFINED/CODE/index.js
@@ -0,0 +1,5 @@
+console.log(a);
+// undefiend
+// js engine gives the speciall placeholder for thos variable which are in the memory even before reading
+// memory allocation is already given to var a but not has the value which is give after the reading this line
+var a = 10;

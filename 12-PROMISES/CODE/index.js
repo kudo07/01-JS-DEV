796be86f371fec3f560599026483f9544e2b9d49
kudo07 <vknwl077@gmail.com>
2025-05-19 17:03:56 +0530

    12-promise

--- 12-PROMISES/CODE/index.js
+++ 12-PROMISES/CODE/index.js
@@ -1,7 +1,6 @@
 // PROMISES Interview Question
 
 // synchronous vs asynchronous code
-
 // 1.
 //Synchronous code
 console.log('start');
@@ -13,43 +12,39 @@ console.log('stop');
 console.log('start');
 
 setTimeout(() => {
-  console.log('Subscribe to Roadside Coder');
+  console.log('here i ams');
 }, 2000);
 
-// 3.
-console.log('stop');
+console.log('STOP');
 
-// Asnychronous Example
+// 3 asyncronous example
 console.log('start');
-
-function importantAction(username) {
+function importAction(username) {
   setTimeout(() => {
-    return `Subscribe to ${username}`;
-  }, 1000);
+    return `hello ${username}`;
+  }, 2000);
 }
-
 const message = importantAction('Roadside Coder');
 console.log(message);
 
 console.log('stop');
 
 // 4
+
 // Callback
 
 console.log('start');
-
-function importantAction(username, cb) {
+function importActionOne(username, cb) {
   setTimeout(() => {
-    cb(`Subscribe to ${username}`);
-  }, 1000);
+    cb(`subscribe ${username}`);
+  });
 }
 
-importantAction('Roadside Coder', (message) => {
+importAction('hel', (message) => {
   console.log(message);
 });
 
 console.log('stop');
-
 // 5.
 // Callback Hell
 console.log('start');
@@ -72,11 +67,11 @@ function shareTheVideo(video, cb) {
   }, 1000);
 }
 
-const message = importantAction('Roadside Coder', (message) => {
+const messageOne = importAction('hello', (message) => {
   console.log(message);
-  likeTheVideo('Javascript Interview Questions', (action) => {
+  likeTheVideo('js', (action) => {
     console.log(action);
-    shareTheVideo('Javascript Interview Questions', (action) => {
+    shareTheVideo('jsq', (action) => {
       console.log(action);
     });
   });
@@ -88,33 +83,36 @@ console.log('stop');
 // Promises
 
 console.log('start');
-
 const sub = new Promise((resolve, reject) => {
   setTimeout(() => {
     const result = true;
-    if (result) resolve('Subscribed to Roadside Coder');
-    else reject(new Error("Why aren't you subscribed to Roadside Coder?"));
+    if (result) {
+      resolve('this will return: resolve');
+    } else {
+      reject(new Error('this is will return reject'));
+    }
   }, 2000);
 });
 
 sub
-  .then((message) => {
-    console.log(message);
+  .then((res) => {
+    console.log(res);
   })
-  .catch((err) => {
-    console.log(err);
+  .catch((rej) => {
+    console.log(rej);
   });
 
 console.log('stop');
 
 // Rewriting Callbacks
 //7
+
 console.log('start');
 
-function importantAction(username) {
-  return new Promise((resolve, reject) => {
+function importActionPromise(username) {
+  return new Promise((res, rej) => {
     setTimeout(() => {
-      resolve(`Subscribe to ${username}`);
+      res(`subs ${username}`);
     }, 1000);
   });
 }
@@ -135,64 +133,69 @@ function shareTheVideo(video) {
   });
 }
 
-importantAction('Roadside Coder')
+importActionPromise('coder')
   .then((res) => {
     console.log(res);
-    likeTheVideo('Javascript Interview Questions').then((res) => {
+    likeTheVideo('elo').then((res) => {
       console.log(res);
-      shareTheVideo('Javascript Interview Questions').then((res) => {
+      shareTheVideo('glo').then((res) => {
         console.log(res);
       });
     });
   })
-  .catch((err) => console.log(err));
-
-console.log('stop');
+  .catch((err) => {
+    console.log(err);
+  });
 
 // 8
 // Promise Chaining
-
-importantAction('Roadside Coder')
+importAction('Rad')
   .then((res) => {
     console.log(res);
-    return likeTheVideo('Javascript Interview Questions');
+    return likeTheVideo('js');
   })
   .then((res) => {
     console.log(res);
-    return shareTheVideo('Javascript Interview Questions');
+    return shareTheVideo('wef');
   })
   .then((res) => {
     console.log(res);
   });
-
 // 9.
 // Promise Combinator
 // 9.1
 // promise.all()
-console.log('start');
 
+console.log('start');
 Promise.all([
-  importantAction('Roadside Coder'),
-  likeTheVideo('Javascript Interview Questions'),
-  shareTheVideo('Javascript Interview Questions'),
+  importActionPromise('roadside'),
+  likeTheVideo('jk'),
+  shareTheVideo('hel'),
 ])
-  .then((res) => {
-    console.log(res);
-  })
+  .then((res) => console.log(res))
   .catch((err) => {
     console.log(err);
   });
 
 console.log('stop');
 
-// 9.2
+const promise1 = Promise.resolve(3);
+const promise2 = 42;
+const promise3 = new Promise((resolve, reject) => {
+  setTimeout(resolve, 100, 'foo');
+});
+
+Promise.all([promise1, promise2, promise3]).then((values) => {
+  console.log(values);
+});
+// Expected output: Array [3, 42, "foo"]
+
+//// 9.2
 // promise.race()
-console.log('start');
-
 Promise.race([
-  importantAction('Roadside Coder'),
-  likeTheVideo('Javascript Interview Questions'),
-  shareTheVideo('Javascript Interview Questions'),
+  importActionPromise('hfbr'),
+  likeTheVideo('jwrgfe'),
+  shareTheVideo('wefk'),
 ])
   .then((res) => {
     console.log(res);
@@ -200,13 +203,7 @@ Promise.race([
   .catch((err) => {
     console.log(err);
   });
-
-console.log('stop');
-
-// 9.3
-// promise.allSettled()
-console.log('start');
-
+//faster one will first execute
 Promise.allSettled([
   importantAction('Roadside Coder'),
   likeTheVideo('Javascript Interview Questions'),
@@ -220,16 +217,21 @@ Promise.allSettled([
   });
 
 console.log('stop');
+const promise5 = Promise.resolve(3);
+const promise4 = new Promise((resolve, reject) =>
+  setTimeout(reject, 100, 'foo')
+);
+const promises = [promise1, promise2];
 
-// 9.4
-// promise.any()
-console.log('start');
+Promise.allSettled(promises).then((results) =>
+  results.forEach((result) => console.log(result.status))
+);
 
-Promise.any([
-  importantAction('Roadside Coder'),
-  likeTheVideo('Javascript Interview Questions'),
-  shareTheVideo('Javascript Interview Questions'),
-])
+// Expected output:
+// "fulfilled"
+// "rejected"
+// 9.4 PROMISE.ANY()
+Promise.any([importAction('wef'), likeTheVideo('fwf'), shareTheVideo('we')])
   .then((res) => {
     console.log(res);
   })
@@ -241,392 +243,15 @@ console.log('stop');
 
 // 10
 // async & await
-
 const result = async () => {
   try {
-    const message1 = await importantAction('Roadside Coder');
-    // wait for this one then move to next one
+    const message1 = await importActionPromise('qew');
     const message2 = await likeTheVideo('Javascript Interview Questions');
     const message3 = await shareTheVideo('Javascript Interview Questions');
-
     console.log({ message1, message2, message3 });
-  } catch (err) {
-    console.log(err);
+  } catch (error) {
+    console.log(error);
   }
 };
 
 result();
-
-// ----------> Interview Question
-
-// Question 1 : What is Output?
-
-console.log('start');
-
-const promise1 = new Promise((resolve, reject) => {
-  console.log(1);
-  resolve(2);
-});
-
-promise1.then((res) => {
-  console.log(res);
-});
-
-console.log('end');
-
-// Question 2 : What is Output?
-
-console.log('start');
-
-const promise1 = new Promise((resolve, reject) => {
-  console.log(1);
-  resolve(2);
-  console.log(3);
-});
-
-promise1.then((res) => {
-  console.log(res);
-});
-
-console.log('end');
-
-// Question 3 : What is Output?
-
-console.log('start');
-
-const fn = () =>
-  new Promise((resolve, reject) => {
-    console.log(1);
-    resolve('success');
-  });
-
-console.log('middle');
-
-fn().then((res) => {
-  console.log(res);
-});
-
-console.log('end');
-
-// Question 4 : What is Output?
-
-function job() {
-  return new Promise(function (resolve, reject) {
-    reject();
-  });
-}
-
-let promise = job();
-
-promise
-
-  .then(function () {
-    console.log('Success 1');
-  })
-
-  .then(function () {
-    console.log('Success 2');
-  })
-
-  .then(function () {
-    console.log('Success 3');
-  })
-
-  .catch(function () {
-    console.log('Error 1');
-  })
-
-  .then(function () {
-    console.log('Success 4');
-  });
-// success
-// error
-// error caught
-//
-// Question 5 : What is Output?
-
-function job(state) {
-  return new Promise(function (resolve, reject) {
-    if (state) {
-      resolve('success');
-    } else {
-      reject('error');
-    }
-  });
-}
-
-let promise = job(true);
-
-promise
-  .then(function (data) {
-    console.log(data);
-
-    return job(false);
-  })
-
-  .catch(function (error) {
-    console.log(error);
-
-    return 'Error caught';
-  })
-
-  .then(function (data) {
-    console.log(data);
-
-    return job(true);
-  })
-
-  .catch(function (error) {
-    console.log(error);
-  });
-
-// Question 6 : What is the Output?
-
-function job(state) {
-  return new Promise(function (resolve, reject) {
-    if (state) {
-      resolve('success');
-    } else {
-      reject('error');
-    }
-  });
-}
-
-let promise = job(true);
-
-promise
-  .then(function (data) {
-    console.log(data);
-
-    return job(true);
-  })
-  .then(function (data) {
-    if (data !== 'victory') {
-      throw 'Defeat';
-    }
-    return job(true);
-  })
-  .then(function (data) {
-    console.log(data);
-  })
-  .catch(function (error) {
-    console.log(error);
-    return job(false);
-  })
-  .then(function (data) {
-    console.log(data);
-    return job(true);
-  })
-  .catch(function (error) {
-    console.log(error);
-    return 'Error caught';
-  })
-  .then(function (data) {
-    console.log(data);
-    return new Error('test'); // Not Returning a promise
-  })
-  .then(function (data) {
-    console.log('Success:', data.message);
-  })
-  .catch(function (data) {
-    console.log('Error:', data.message);
-  });
-
-// Question 7 : Promises Chaining
-
-const firstPromise = new Promise((resolve, reject) => {
-  resolve('First!');
-});
-
-const secondPromise = new Promise((resolve, reject) => {
-  resolve(firstPromise);
-});
-
-secondPromise.then((res) => res).then((res) => console.log(res));
-
-// Question 8 : Rewrite this example code using `async/await`
-//  Instead of `.then/catch`
-
-function loadJson(url) {
-  return fetch(url).then((response) => {
-    if (response.status == 200) {
-      return response.json();
-    } else {
-      throw new Error(response.status);
-    }
-  });
-}
-
-loadJson('https://javascript.info/no-such-user.json').catch(alert);
-
-// Answer
-async function loadJson(url) {
-  // (1)
-  let response = await fetch(url); // (2)
-
-  if (response.status == 200) {
-    let json = await response.json(); // (3)
-    return json;
-  }
-
-  throw new Error(response.status);
-}
-
-loadJson('https://javascript.info/no-such-user.json').catch(alert);
-
-// Question 9 : Solve Promise Recursively
-
-function importantAction(username) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve(`Subscribe to ${username}`);
-    }, 1000);
-  });
-}
-
-function likeTheVideo(video) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve(`Like the ${video} video`);
-    }, 1000);
-  });
-}
-
-function shareTheVideo(video) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve(`Share the ${video} video`);
-    }, 1000);
-  });
-}
-
-function promRecurse(funcPromises) {
-  if (funcPromises.length === 0) return;
-  const promi = funcPromises.shift();
-
-  promi.then((result) => console.log(result)).catch((err) => console.log(err));
-
-  promRecurse(funcPromises);
-}
-
-promRecurse([
-  importantAction('Roadside Coder'),
-  likeTheVideo('Javascript Interview Questions'),
-  shareTheVideo('Javascript Interview Questions'),
-]);
-
-// Question 10 : Promise Polyfill
-
-function PromisePolyFill(executor) {
-  let onResolve,
-    onReject,
-    fulfilled = false,
-    rejected = false,
-    called = false,
-    value;
-
-  function resolve(v) {
-    fulfilled = true;
-    value = v;
-
-    if (typeof onResolve === 'function') {
-      // for async
-      console.log('inside resolve');
-      onResolve(value);
-      called = true;
-    }
-  }
-
-  function reject(reason) {
-    rejected = true;
-    value = reason;
-
-    if (typeof onReject === 'function') {
-      onReject(value);
-      called = true;
-    }
-  }
-
-  this.then = function (callback) {
-    onResolve = callback;
-
-    if (fulfilled && !called) {
-      // for sync
-      console.log('inside then');
-      called = true;
-      onResolve(value);
-    }
-    return this;
-  };
-
-  this.catch = function (callback) {
-    onReject = callback;
-
-    if (rejected && !called) {
-      called = true;
-      onReject(value);
-    }
-    return this;
-  };
-
-  try {
-    executor(resolve, reject);
-  } catch (error) {
-    reject(error);
-  }
-}
-
-const promise1 = new PromisePolyFill((resolve, reject) => {
-  console.log(1);
-  setTimeout(() => {
-    resolve(2);
-  }, 1000);
-  console.log(3);
-});
-
-promise1.then((res) => {
-  console.log(res);
-});
-
-const examplePromise = new PromisePolyFill((resolve, reject) => {
-  resolve(2);
-});
-
-examplePromise
-  .then((res) => console.log(res))
-  .catch((err) => console.error(err));
-
-PromisePolyFill.resolve = (val) =>
-  new PromisePolyFill(function executor(resolve, reject) {
-    resolve(val);
-  });
-
-PromisePolyFill.reject = (val) =>
-  new PromisePolyFill(function executor(resolve, reject) {
-    reject(val);
-  });
-
-// promise.all()
-
-PromisePolyFill.all = (promises) => {
-  let fulfilledPromises = [],
-    result = [];
-
-  function executor(resolve, reject) {
-    promises.forEach((promise, index) =>
-      promise
-        .then((val) => {
-          fulfilledPromises.push(true);
-          result[index] = val;
-
-          if (fulfilledPromises.length === promises.length) {
-            return resolve(result);
-          }
-        })
-        .catch((error) => {
-          return reject(error);
-        })
-    );
-  }
-  return new PromisePolyFill(executor);
-};
